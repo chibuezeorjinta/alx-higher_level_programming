396c0e9d5ea97fba2aabdbd22e41485c5d1c9250
chibueze orjinta <chibueze.orjinta.237245@unn.edu.ng>
2023-01-09 13:41:32 +0000

    task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Gets a list of active members in a class.
+
+	obj = Given input
+
+	Return: a list of objects
+"""
+def lookup(obj):
+	"""Got indent to work"""
+	return(dir(obj))
