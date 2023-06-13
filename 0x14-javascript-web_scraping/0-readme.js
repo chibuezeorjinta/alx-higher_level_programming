4b2cffb5ae47467b66d3f27bf718baeb34f2c75f
danielorjinta <chibueze.orjinta.237245@unn.edu.ng>
2023-06-13 17:26:54 +0000

    web scraping task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const file = require('fs');
+
+file.readFile(process.argv[2], 'utf-8', function (err, out) {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log(out);
+	}
+});
